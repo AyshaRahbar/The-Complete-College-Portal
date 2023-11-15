c1b8481a73851e7ced687a5eba680ddd94ba5f05
Aysha Rahbar <135313114+AyshaRahbar@users.noreply.github.com>
2023-11-15 22:13:31 +0530

    Add files via upload

---
+++ DATA/Subject.java
@@ -0,0 +1,12 @@
+package com.project.project.attendance.data;
+
+public class Subject {
+
+    public static String subjects = "subjects";
+
+    public static String labSubs = "labSubs";
+
+    public static String objectId = "objectId";
+
+
+}
