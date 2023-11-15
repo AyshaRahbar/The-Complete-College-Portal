28118ba7b0d41fbdcb0bfd27df290c237d9a2e08
Aysha Rahbar <135313114+AyshaRahbar@users.noreply.github.com>
2023-11-15 22:03:01 +0530

    Add files via upload

---
+++ Faculty.java
@@ -0,0 +1,21 @@
+package com.project.project.attendance.data;
+
+public class Faculty {
+
+    public static String objectId = "objectId";
+
+    public static String username = "username";
+
+    public static String password = "password";
+
+    public static String name = "name";
+
+    public static String qualification = "qualification";
+
+    public static String address = "address";
+
+    public static String phone = "phone";
+
+    public static String subjects = "subjects";
+
+}
